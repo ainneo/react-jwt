5d39462c625c65bc9505fb1767890eb2b70bf1af
AO <ainneo@gmail.com>
2021-08-22 22:21:15 -0700

    add user auth login and out functions

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 - React JWT AUTH: https://www.youtube.com/watch?v=OUP-urBy1k4&t=0s
 - Backend: Build a fake backend with Docker: https://github.com/scalablescripts/auth-api
+
   - run docker-compose up to build containers, then close containers to and run docker-compose up - d, and run the last command: docker exec backend /bin/sh start.sh (this command builds the mock database).
   - now the backend is running on localhost:8000
+
+  -\*\*\* for other backends: https://www.youtube.com/watch?v=OUP-urBy1k4
---
+++ src/components/Nav.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Nav(props) {
+  const logout = async () => {
+    await fetch("http://localhost:8000/api/logout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+
+    props.setName("");
+  };
+
+  let menu;
+
+  if (props.name === "") {
+    menu = (
+      <ul className="navbar-nav me-auto mb-2 mb-md-0">
+        <li className="nav-item active">
+          <Link to="/login" className="nav-link">
+            Login
+          </Link>
+        </li>
+        <li className="nav-item active">
+          <Link to="/register" className="nav-link">
+            Register
+          </Link>
+        </li>
+      </ul>
+    );
+  } else {
+    menu = (
+      <ul className="navbar-nav me-auto mb-2 mb-md-0">
+        <li className="nav-item active">
+          <Link to="/login" className="nav-link" onClick={logout}>
+            Logout
+          </Link>
+        </li>
+      </ul>
+    );
+  }
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+        <div>
+          <Link className="navbar-brand" to="/">
+            Fixed navbar
+          </Link>
+        </div>
+
+        <div>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">{menu}</ul>
+        </div>
+      </nav>
+    </>
+  );
+}
